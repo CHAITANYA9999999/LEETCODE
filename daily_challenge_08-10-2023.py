bdba76b0bfb805b7d8e333826ff542c4e261bd3e
CHAITANYA9999999 <chiknum@gmail.com>
2023-10-08 23:31:54 +0530

    daily challenge 08-10-2023

--- daily_challenge_08-10-2023.py
+++ daily_challenge_08-10-2023.py
@@ -1,30 +1,13 @@
 class Solution:
-    def numOfArrays(self, n: int, m: int, k: int) -> int:
-        mod = 10**9 + 7
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        m, n = len(nums1), len(nums2)
         
-        dp = [[0] * (k+1) for _ in range(m+1)]
-        prefix = [[0] * (k+1) for _ in range(m+1)]
-        prevDp = [[0] * (k+1) for _ in range(m+1)]
-        prevPrefix = [[0] * (k+1) for _ in range(m+1)]
+        dp = [[float('-inf')] * (n + 1) for _ in range(m + 1)]
         
-        for j in range(1, m+1):
-            prevDp[j][1] = 1
-            prevPrefix[j][1] = j
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                curr_product = nums1[i-1] * nums2[j-1]
+                
+                dp[i][j] = max(dp[i][j], curr_product, dp[i-1][j], dp[i][j-1], curr_product + dp[i-1][j-1])
         
-        for _ in range(2, n+1):
-            dp = [[0] * (k+1) for _ in range(m+1)]
-            prefix = [[0] * (k+1) for _ in range(m+1)]
-            
-            for maxNum in range(1, m+1):
-                for cost in range(1, k+1):
-                    dp[maxNum][cost] = (maxNum * prevDp[maxNum][cost]) % mod
-                    
-                    if maxNum > 1 and cost > 1:
-                        dp[maxNum][cost] += prevPrefix[maxNum - 1][cost - 1]
-                        dp[maxNum][cost] %= mod
-                    
-                    prefix[maxNum][cost] = (prefix[maxNum - 1][cost] + dp[maxNum][cost]) % mod
-            
-            prevDp, prevPrefix = [row[:] for row in dp], [row[:] for row in prefix]
-        
-        return prefix[m][k]
+        return dp[m][n]
