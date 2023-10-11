f686e8bf571e150bedb9ba35251f48833183c97a
CHAITANYA9999999 <chiknum@gmail.com>
2023-10-11 21:59:32 +0530

    daily challenge 11-10-2023

--- daily_challenge_11-10-2023.py
+++ daily_challenge_11-10-2023.py
@@ -1,5 +1,7 @@
+import bisect
+
 class Solution(object):
     def fullBloomFlowers(self, flowers, people):
         start = sorted([a for a,b in flowers])
         end = sorted([b for a,b in flowers])
-        return [bisect_right(start,i) - bisect_left(end,i) for i in people]
+        return [bisect.bisect_right(start,i) - bisect.bisect_left(end,i) for i in people]
