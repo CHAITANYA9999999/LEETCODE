92423ce01257afbc15d1d3a0edc122be5d1215bd
Chaitanya Mahajan <98775345+CHAITANYA9999999@users.noreply.github.com>
2024-07-22 21:10:26 +0530

    Time: 97 ms (88.92%), Space: 17.1 MB (14.55%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,3 +1,3 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        return reversed([x for _,x in sorted(zip(heights,names))])
+        return [x for _,x in sorted(zip(heights,names),reverse = True)]
