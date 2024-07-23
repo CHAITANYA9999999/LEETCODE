6aa856687fdc132da67fa8e260153f23c9b41782
Chaitanya Mahajan <98775345+CHAITANYA9999999@users.noreply.github.com>
2024-07-23 21:03:14 +0530

    Time: 35 ms (34.91%), Space: 11.8 MB (17.67%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
@@ -1,4 +1,3 @@
-import bisect
 class Solution(object):
     def frequencySort(self, nums):
         freq = collections.Counter(nums)
@@ -7,9 +6,9 @@ class Solution(object):
             if frequency not in freqs:
                 freqs[frequency] = []
             print(num,frequency)
-            freqs[frequency].append(num)
+            bisect.insort(freqs[frequency],num)
         res = []
         for frequency in sorted(freqs.keys()):
-            for num in sorted(freqs[frequency],reverse = True):
+            for num in reversed(freqs[frequency]):
                 res.extend([num]*frequency)
         return res
