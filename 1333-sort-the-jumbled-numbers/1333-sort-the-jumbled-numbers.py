5427ece82182924a6ca2cc4295c0f788223408a2
Chaitanya Mahajan <98775345+CHAITANYA9999999@users.noreply.github.com>
2024-07-24 19:32:05 +0530

    Time: 1314 ms (14.13%), Space: 25.7 MB (31.52%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -7,7 +7,5 @@ class Solution:
             for i in range(len(str_num)):
                 new_num += str(mapping[int(str_num[i])])
             res.append(int(new_num))
-        zipped = []
-        for i in range(len(nums)):
-            zipped.append([res[i],i,nums[i]])
-        return [num for _,_,num in sorted(zipped)]
+        print(res)
+        return [num for _,_,num in sorted([[res[i],i,nums[i]] for i in range(len(nums))])]
